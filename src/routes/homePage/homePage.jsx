ca060f343044c75b3dcc0e96d5717054674920c0
EngNelson <bechem200@gmail.com>
2024-04-26 15:45:30 +0100

    home page

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
 import './homePage.scss'
+import SearchBar from '../../components/searchBar/SearchBar'
 
 const homePage = () => {
   return (
     <div className='homePage'>
         <div className="textContainer">
             <div className="wrapper">
-           <h1>Find Real Estate & Get Your Dream Place Find Real Estate & Get 
-            Your Dream Place
+           <h1 className="title">Find Real Estate & Get Your Dream Place
            </h1>
+           <p>
+           Place Find Real Estate & Get 
+            Your Dream Place
+           </p>
+
+           <SearchBar/>
+               <div className="boxes">
+                <div className="box">
+                    <h1>16+</h1>
+                    <h2>Years of Experience</h2>
+                </div>
+                <div className="box">
+                    <h1>200</h1>
+                    <h2>Award Gained</h2>
+                </div>
+                <div className="box">
+                    <h1>20000+</h1>
+                    <h2>Property Ready</h2>
+                </div>
+               </div>
            </div>
         </div>
         <div className="imgContainer">
