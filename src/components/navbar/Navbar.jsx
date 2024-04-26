05bf0714a996dc39290a6372a910e052dd391a7c
EngNelson <bechem200@gmail.com>
2024-04-26 14:21:46 +0100

    home page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./navbar.scss";
 
 const Navbar = () => {
+  const [open,setOpen] = useState(false)
   return (
     <nav>
         <div className="left">
           <a href="/" className="logo">
             <img src="/logo.png" alt="" />
-            <span>Indomintable Real Estate</span>
+            <span>Indomintable Estate</span>
           </a>
           <a href="/">Home</a>
           <a href="/">About</a>
@@ -16,11 +17,13 @@ const Navbar = () => {
         </div>
         <div className="right">
         <a href="/">Sign in</a>
-        <a href="/" className="register">Sign up</a>
+        <a href="/" className="register">
+          Sign up
+          </a>
         <div className="menuIcon">
-          <img src="/menu.png" alt="" />
+          <img src="/menu.png" alt="" onClick={()=>setOpen((prev) => !prev)}/>
         </div>
-        <div className="menu">
+        <div className={open ? "menu active" : "menu"}>
         <a href="/">Home</a>
           <a href="/">About</a>
           <a href="/">Contact</a>
